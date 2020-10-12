e4f5b7dd6323bf8c60ccb491e6f5af7bea88ad52
vagrant <vagrant@vagrant.vm>
2020-10-12 19:40:14 +0000

    ALL TESTS PASSING.

--- desserts.py
+++ desserts.py
@@ -4,6 +4,43 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
+    def __init__(self, name, flavor, price):
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+
+        self.cache[self.name] = self
+
+    def add_stock(self, amount):
+        self.qty = amount
+
+    def sell(self, amount):
+        if self.qty == 0:
+            print('Sorry, these cupcakes are sold out')
+        else:
+            if not self.qty <= amount:
+                self.qty = self.qty - amount
+            else:
+                self.qty = 0
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+        list_of_new_ingredients = []
+        for ingredient, qty in ingredients:
+            new_qty = qty * amount 
+            list_of_new_ingredients.append((ingredient, new_qty))
+        return list_of_new_ingredients
+
+    @classmethod
+    def get(cls, name):
+        if name in cls.cache:
+            return cls.cache[name]
+        else:
+            print("Sorry, that cupcake doesn't exist")
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
